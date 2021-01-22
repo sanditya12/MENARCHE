13b42707dde99f3fa78a277951337aaf3d7ad446
sanditya <sanditya2002@gmail.com>
2021-01-22 17:28:41 +0700

    v1

---
+++ index.js
@@ -0,0 +1,9 @@
+function handleOpenSidebar() {
+  document.getElementById("page").style.right = "70vw";
+  document.getElementById("sidebar").style.left = "30vw";
+}
+
+function handleCloseSidebar() {
+  document.getElementById("page").style.right = "0vw";
+  document.getElementById("sidebar").style.left = "100vw";
+}
